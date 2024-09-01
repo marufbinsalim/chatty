31cec9d54a5ec63b1d5d28dea847933a716a6a74
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2024-09-01 22:38:30 +0600

    updated the content for the homeScreen

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -8,12 +8,14 @@ export default function HomeScreen() {
         backgroundImage: "url('/world.jpg')",
       }}
     >
-      <h1 className="text-5xl text-white font-bold uppercase">ChattyPals</h1>
+      <h1 className="text-3xl md:text-5xl text-white font-bold">ChattyPals</h1>
+      <p className="text-white text-center mt-4 max-w-md md:text-2xl">
+        A place to connect with people from all around the world.
+        <br />
+      </p>
       <SignInButton>
-        <button className="text-white">
-          <p>
-            <span className="text-2xl underline">Sign in</span> to get started
-          </p>
+        <button className="p-2 px-4 rounded-3xl bg-white mt-6">
+          Get Started
         </button>
       </SignInButton>
     </div>
