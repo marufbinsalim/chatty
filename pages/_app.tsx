aa2929cf9040867f9ee5b404c97c2c6d37ee0c9a
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2024-09-01 21:08:41 +0600

    boilerplate init

---
+++ pages/_app.tsx
@@ -0,0 +1,24 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import "@/styles/globals.css";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ClerkProvider>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
+}
