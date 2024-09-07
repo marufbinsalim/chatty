03c4009bc7c863c95090151d70952f77605d2d88
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2024-09-08 02:09:04 +0600

    basic profile functionality completed

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -7,6 +7,7 @@ import {
   Edit,
   Edit2Icon,
   EditIcon,
+  InfoIcon,
   LucideAlignHorizontalDistributeCenter,
   Save,
   StepBack,
@@ -29,26 +30,171 @@ function ProfileInfoCard({
   bio: string | null;
 }) {
   return (
-    <div className="flex flex-col items-center bg-red-300 flex-1 p-4">
+    <div className="flex flex-col flex-1 p-4">
       {imageURL && (
         <img
           src={imageURL}
           alt="profile"
-          className="rounded-full h-20 mx-auto"
+          className="rounded-full h-20 w-20 mx-auto"
         />
       )}
       {firstName && lastName && (
-        <p className="w-full text-center">
+        <p className="w-full text-center text-2xl font-bold">
           {firstName} {lastName}
         </p>
       )}
-      {email && <p className="w-full text-center">{email}</p>}
-      {bio && <p className="w-full text-center">{bio}</p>}
+      {email && <p className="w-full text-center text-gray-700">{email}</p>}
+      {bio && (
+        <div className="mt-12 md:mx-auto">
+          <p className="w-full text-2xl font-bold">Bio</p>
+          <p className="w-max  max-w-full max-h-[300px] overflow-auto bg-fuchsia-50 py-4 px-2">
+            {bio}
+          </p>
+        </div>
+      )}
       <SignOutButton>
-        <button className="bg-red-500 text-white p-2 rounded-md mt-auto">
+        <button className="bg-red-500 text-white p-2 rounded-md mt-4 w-max md:mx-auto">
           Sign Out
         </button>
       </SignOutButton>
+    </div>
+  );
+}
+
+function EditToggleButton({
+  editing,
+  setEditing,
+}: {
+  editing: boolean;
+  setEditing: (editing: boolean) => void;
+}) {
+  return (
+    <button
+      onClick={() => {
+        setEditing(!editing);
+      }}
+    >
+      {editing ? <XIcon /> : <EditIcon />}
+    </button>
+  );
+}
+
+function EditInformationCard({
+  user,
+  setEditing,
+  imageUrl,
+  firstName,
+  lastName,
+  bio,
+  setFirstName,
+  setLastName,
+  setBio,
+  email,
+}: {
+  user: any;
+  setEditing: (editing: boolean) => void;
+  imageUrl: string | null;
+  firstName: string;
+  lastName: string;
+  bio: string;
+  setFirstName: (firstName: string) => void;
+  setLastName: (lastName: string) => void;
+  setBio: (bio: string) => void;
+  email: string | null;
+}) {
+  const [image, setImage] = useState<File | null>(null);
+
+  useEffect(() => {
+    async function updateImage() {
+      user
+        ?.setProfileImage({ file: image })
+        .then(() => {
+          user.reload();
+        })
+        .catch((e: any) => console.log(e));
+    }
+    if (image) {
+      updateImage();
+    }
+  }, [image]);
+
+  async function saveChanges() {
+    await user?.update({
+      firstName,
+      lastName,
+      unsafeMetadata: { bio },
+    });
+    user.reload();
+    setEditing(false);
+  }
+
+  return (
+    <div className="flex flex-col items-center flex-1 p-4">
+      <div className="flex flex-col items-center w-full md:w-max">
+        <div className="relative">
+          {/* Hidden file input */}
+          <input
+            type="file"
+            accept="image/*"
+            id="file-input"
+            className="absolute inset-0 opacity-0 cursor-pointer"
+            onChange={(e) => {
+              if (e.target.files?.[0]) setImage(e.target.files?.[0]);
+            }}
+          />
+
+          {/* Custom round button styled as an image icon */}
+          <label
+            htmlFor="file-input"
+            className="flex items-center justify-center w-20 h-20 rounded-full bg-gray-100 cursor-pointer overflow-hidden"
+          >
+            <img src={imageUrl || ""} alt="Selected" className="w-20 h-20" />
+          </label>
+        </div>
+
+        {email && (
+          <input
+            type="text"
+            placeholder="Email"
+            className="w-full md:w-[400px] p-2 mt-4 mb-2 bg-gray-100 border-[1px] border-gray-300 rounded-lg text-gray-500"
+            value={email}
+            disabled
+          />
+        )}
+        <input
+          type="text"
+          placeholder="First Name"
+          className="w-full md:w-[400px] p-2 my-2 bg-gray-100 border-[1px] border-gray-300 rounded-lg"
+          value={firstName}
+          onChange={(e) => {
+            setFirstName(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Last Name"
+          className="w-full md:w-[400px] p-2 my-2 bg-gray-100 border-[1px] border-gray-300 rounded-lg"
+          value={lastName}
+          onChange={(e) => {
+            setLastName(e.target.value);
+          }}
+        />
+
+        <textarea
+          value={bio}
+          onChange={(e) => {
+            setBio(e.target.value);
+          }}
+          placeholder="Bio"
+          className="w-full md:w-[400px] h-32 p-2 my-2 bg-gray-100 border-[1px] border-gray-300 rounded-lg"
+        ></textarea>
+        <button
+          className="bg-green-500 text-white p-2 rounded-md m-auto mt-4"
+          onClick={saveChanges}
+        >
+          Save
+        </button>
+      </div>
     </div>
   );
 }
@@ -71,7 +217,7 @@ export default function Profile() {
     // @ts-ignore
     setBio(user.unsafeMetadata["bio"]);
     setImageUrl(user.imageUrl);
-  }, [user]);
+  }, [user, editing]);
 
   if (!isLoaded || !user) {
     return <div>Loading...</div>;
@@ -83,25 +229,26 @@ export default function Profile() {
         <Link href="/">
           <ChevronLeft size={32} />
         </Link>
-        <p> My Profile</p>
-        {editing ? (
-          <XIcon
-            onClick={() => {
-              setEditing(false);
-            }}
-            size={24}
-          />
-        ) : (
-          <EditIcon
-            onClick={() => {
-              setEditing(true);
-            }}
-            size={24}
-          />
-        )}
+        <p className="text-2xl font-bold">
+          {" "}
+          {editing ? "Edit Profile" : "My Profile"}
+        </p>
+
+        <EditToggleButton editing={editing} setEditing={setEditing} />
       </div>
       {editing && (
-        <p className="bg-red-300 p-4"> You are editing your profile </p>
+        <EditInformationCard
+          user={user}
+          setEditing={setEditing}
+          imageUrl={imageUrl}
+          firstName={firstName || ""}
+          lastName={lastName || ""}
+          bio={bio || ""}
+          setFirstName={setFirstName}
+          setLastName={setLastName}
+          setBio={setBio}
+          email={user.emailAddresses[0].emailAddress}
+        />
       )}
       {!editing && (
         <ProfileInfoCard
@@ -115,25 +262,3 @@ export default function Profile() {
     </div>
   );
 }
-
-// <button
-//   onClick={() => {
-//     user
-//       .update({
-//         firstName: "John",
-//         lastName: "Doe",
-//         unsafeMetadata: {
-//           bio: "oh no!",
-//         },
-//       })
-//       .then(() => {
-//         console.log("First name updated");
-//         user.reload();
-//       })
-//       .catch((error) => {
-//         console.error("Error updating first name:", error);
-//       });
-//   }}
-// >
-//   {"Change first name to John"}
-// </button>
