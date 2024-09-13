430946d5ee408a88294374fba6c837f67a9cb6ef
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2024-09-14 00:24:48 +0600

    debounced query done

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -1,34 +1,56 @@
+import useDiscover from "@/hooks/useDiscover";
 import { supabase } from "@/pages/utils/supabase/uiClient";
+import {
+  ChevronLeft,
+  ChevronLeftIcon,
+  ChevronRight,
+  ChevronsRight,
+} from "lucide-react";
 import { useEffect, useState } from "react";
 
 export default function Discover() {
-  const [users, setUsers] = useState<any[]>([]);
-  const [loading, setLoading] = useState(false);
+  const [query, setQuery] = useState<string>("");
 
-  useEffect(() => {
-    async function fetchUsers() {
-      try {
-        setLoading(true);
-        const { data, error } = await supabase
-          .from("users")
-          .select("*")
-          .range(0, 10);
-        if (error) throw error;
-        setUsers(data);
-      } catch (error) {
-        console.error("Error fetching users", error);
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    fetchUsers();
-  }, []);
+  const { users, loading, totalPage, currentPage, nextPage, previousPage } =
+    useDiscover({
+      query: query,
+    });
 
   return (
     <div className="flex-1 bg-red-50">
       <h1>Discover</h1>
-      {JSON.stringify(users)}
+      <div>
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Search users"
+        />
+      </div>
+
+      {totalPage !== 0 && (
+        <div className="flex gap-4 ml-auto mr-4 w-max">
+          <button
+            disabled={currentPage === 1}
+            onClick={previousPage}
+            className="disabled:opacity-50"
+          >
+            <ChevronLeft />
+          </button>
+          <p>{`${currentPage}/${totalPage}`}</p>
+          <button
+            onClick={nextPage}
+            disabled={currentPage === totalPage}
+            className="disabled:opacity-50"
+          >
+            <ChevronRight />
+          </button>
+        </div>
+      )}
+      {loading && <div>Loading...</div>}
+      <p className="text-sm text-gray-500 break-words">
+        {JSON.stringify(users)}
+      </p>
     </div>
   );
 }
---
+++ hooks/useDiscover.tsx
@@ -0,0 +1,69 @@
+import { supabase } from "@/pages/utils/supabase/uiClient";
+import { useUser } from "@clerk/nextjs";
+import { useEffect, useState } from "react";
+
+const PAGE_SIZE = 1;
+export default function useDiscover({ query }: { query: string }) {
+  const [users, setUsers] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(1);
+  const [debouncedQuery, setDebouncedQuery] = useState(query);
+  const { user } = useUser();
+
+  useEffect(() => {
+    console.log("query", query);
+
+    async function fetchUsers() {
+      console.log("fetching users");
+      try {
+        setLoading(true);
+        const { data, error, count } = await supabase
+          .from("users")
+          .select("*", { count: "exact" })
+          .ilike("user_name", `%${query}%`)
+          .neq("id", user?.id)
+          .range((currentPage - 1) * PAGE_SIZE, currentPage * PAGE_SIZE - 1);
+
+        if (error) throw error;
+
+        setUsers(data);
+        if (typeof count !== "number") throw new Error("Count is not a number");
+        setTotalPage(Math.ceil(count / PAGE_SIZE));
+      } catch (error) {
+        console.error("Error fetching users", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    if (user) fetchUsers();
+  }, [user, currentPage, debouncedQuery]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setCurrentPage(1);
+      setDebouncedQuery(query);
+    }, 700);
+
+    return () => clearTimeout(timeout);
+  }, [query]);
+
+  async function searchForUsers() {}
+
+  async function nextPage() {
+    setCurrentPage(currentPage + 1);
+  }
+  async function previousPage() {
+    setCurrentPage(currentPage - 1);
+  }
+
+  return {
+    users,
+    loading,
+    searchForUsers,
+    nextPage,
+    previousPage,
+    currentPage,
+    totalPage,
+  };
+}
